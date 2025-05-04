454be40296592de26557f4826f6c0dd8aa85a5a2
eduardafs7 <eduarda.freire@a.ucb.br>
2025-05-04 15:22:33 -0300

    Update exercicio1.c

--- exercicio1.c
+++ exercicio1.c
@@ -1,1 +1,10 @@
+#include <stdio.h>
 
+int main() {
+    int numero = 9;  // Número fixo
+
+    // Exibe o antecessor do número
+    printf("O antecessor de %d é %d.\n", numero, numero - 1);
+
+    return 0;
+}
